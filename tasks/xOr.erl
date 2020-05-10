6a1bd7633ab205683027ca16bbf7708a221382c3
Oleksandr Bratashov <abratashov@gmail.com>
2020-05-10 18:51:06 +0300

    Add Xor task

---
+++ tasks/xOr.erl
@@ -0,0 +1,26 @@
+-module(xOr).
+
+-export([xOr1/2, xOr2/2, xOr3/2, test/0]).
+
+xOr1(A, B) ->
+    A =/= B.
+
+xOr2(A, B) ->
+    not (A == B).
+
+xOr3(A, B) ->
+    (A == true) and (B == false) or
+    (A == false) and (B == true).
+
+% Tests
+
+testFun(XorFun) ->
+    (XorFun(true, true) == false) and
+    (XorFun(true, false) == true) and
+    (XorFun(false, true) == true) and
+    (XorFun(false, false) == false).
+
+test() ->
+    testFun(fun xOr1/2) and
+    testFun(fun xOr2/2) and
+    testFun(fun xOr3/2).
