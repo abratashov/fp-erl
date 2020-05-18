07ebe1ddc05aed16f5aec292c675f78de6ae0ff3
Oleksandr Bratashov <abratashov@gmail.com>
2020-05-18 21:39:27 +0300

    Remove not needed equal function

--- tasks/lists/palindrome.erl
+++ tasks/lists/palindrome.erl
@@ -2,7 +2,6 @@
 -export([
   reverse/1,
   reverse/2,
-  equal/2,
   is_palindrome/1,
   test/0
 ]).
@@ -12,14 +11,8 @@ reverse(L) -> reverse(L, []).
 reverse([], Result) -> Result;
 reverse([H|T], Result) -> reverse(T, [H|Result]).
 
-equal([], []) -> true;
-equal([], [_|_]) -> false;
-equal([_|_], []) -> false;
-equal([H1|_], [H2|_]) when H1 =/= H2 -> false;
-equal([_|T1], [_|T2]) -> equal(T1, T2).
-
 is_palindrome([]) -> true;
-is_palindrome(L) -> equal(L, reverse(L)).
+is_palindrome(L) -> L == reverse(L).
 
 % Tests
 
@@ -28,19 +21,6 @@ test_reverse() ->
   [1] = reverse([1]),
   [2,1] = reverse([1,2]),
   [5,4,3,2,1] = reverse([1,2,3,4,5]),
-  ok.
-
-test_equal() ->
-  true = equal([], []),
-  true = equal([1],[1]),
-  true = equal([1,2], [1,2]),
-  true = equal([1,2,3,4,5], [1,2,3,4,5]),
-  false = equal([], [1]),
-  false = equal([1], [0]),
-  false = equal([1,2], [1,3]),
-  false = equal([1,2], [1,3,4]),
-  false = equal([1,2,3], [1,3]),
-  false = equal([1,3], [1,3,4]),
   ok.
 
 test_is_palindrome() ->
@@ -56,6 +36,5 @@ test_is_palindrome() ->
 
 test() ->
   ok = test_reverse(),
-  ok = test_equal(),
   ok = test_is_palindrome(),
   ok.
