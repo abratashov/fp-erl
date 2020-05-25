2bbf718b318b0b2d57ff7e449a3ed22ff5605f21
Oleksandr Bratashov <abratashov@gmail.com>
2020-05-25 20:03:27 +0300

    Add higher-order functions

--- tasks/higher_order_func.erl
+++ tasks/higher_order_func.erl
@@ -6,16 +6,21 @@
   test/0
 ]).
 
-double_all([]) -> [];
-double_all([X|Xs]) -> [ 2*X | double_all(Xs) ].
+double_all(L) -> lists:map(fun(Num) -> 2*Num end, L).
 
-evens([]) -> [];
-evens([X|Xs]) when X rem 2 == 0 -> [X | evens(Xs) ];
-evens([_|Xs]) -> evens(Xs).
+evens(L) -> lists:filter(fun(Num) -> Num rem 2 == 0 end, L).
 
-product([]) -> 1;
-product([X|Xs]) -> X * product(Xs).
+product(L) -> lists:foldr(fun(Num, Result) -> Result*Num end, 1, L).
 
+zip(L1, L2) -> zip(L1, L2, []).
+zip([], _, Result) -> lists:reverse(Result);
+zip(_, [], Result) -> lists:reverse(Result);
+zip([H1|T1], [H2|T2], Result) -> zip(T1, T2, [{H1,H2}|Result]).
+
+zip_with(Func, L1, L2) -> zip_with(Func, L1, L2, []).
+zip_with(Func, [], _, Result) -> lists:reverse(Result);
+zip_with(Func, _, [], Result) -> lists:reverse(Result);
+zip_with(Func, [H1|T1], [H2|T2], Result) -> zip_with(Func, T1, T2, [Func(H1,H2)|Result]).
 % Tests
 
 test_double_all() ->
@@ -33,8 +38,20 @@ test_product() ->
   120 = product([1,2,3,4,5]),
   ok.
 
+test_zip() ->
+  [] = zip([1], []),
+  [{1,2}, {3,4}] = zip([1,3,5,7], [2,4]),
+  ok.
+
+test_zip_with() ->
+  [] = zip_with(fun(X,Y) -> X+Y end, [1], []),
+  [3,7] = zip_with(fun(X,Y) -> X+Y end, [1,3,5,7], [2,4]),
+  ok.
+
 test() ->
   ok = test_double_all(),
   ok = test_evens(),
   ok = test_product(),
+  ok = test_zip(),
+  ok = test_zip_with(),
   ok.
